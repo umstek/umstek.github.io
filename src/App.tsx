0066d01c14a87001bc20b997eed99f265e455eb2
Wickramaranga Abeygunawardhana <umstek@live.com>
2018-09-15 13:26:20 +0530

    Add markdown support

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import * as React from "react";
+// tslint:disable-next-line
+import Markdown from "markdown-to-jsx";
+
 import "spectre.css";
+
+import Test from "./pages/test";
+
+// tslint:disable-next-line
+import { markdown, frontmatter } from "./content/test.md";
 
 export default class App extends React.Component {
   render() {
-    return <div>Hello</div>;
+    // console.log(markdown);
+
+    return (
+      <div>
+        <Test />
+        <Markdown>{markdown}</Markdown>
+      </div>
+    );
   }
 }
---
+++ src/content/test.md
@@ -0,0 +1,7 @@
+---
+path: random-path
+---
+
+# Working?
+
+True
