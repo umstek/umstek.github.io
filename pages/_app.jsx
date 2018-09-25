42b241519cacfa60f2feb4450607dce02c7cc237
Wickramaranga Abeygunawardhana <umstek@live.com>
2018-09-25 12:48:36 +0530

    Rename config to styles

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import { MDXProvider } from "@mdx-js/tag";
 import App, { Container } from "next/app";
 import { Provider as RebassProvider } from "rebass";
-import { ThemeProvider } from "styled-components";
 
-import theme from "../config/theme";
+import theme from "../styles/theme";
 import Layout from "../components/Layout";
 import components from "../components/markdown";
 
@@ -13,13 +12,13 @@ export default class _App extends App {
     const { Component, pageProps } = this.props;
     return (
       <MDXProvider components={components}>
-          <RebassProvider theme={theme}>
-            <Container>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </Container>
-          </RebassProvider>
+        <RebassProvider theme={theme}>
+          <Container>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </Container>
+        </RebassProvider>
       </MDXProvider>
     );
   }
