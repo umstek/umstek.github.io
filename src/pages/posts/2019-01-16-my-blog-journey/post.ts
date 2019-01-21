d8d0db5e7f61c351e4c268dece6894b55f1fe286
Wickramaranga Abeygunawardhana <umstek@live.com>
2019-01-21 22:54:03 +0530

    Add my blog journey 1

---
+++ src/pages/posts/2019-01-16-my-blog-journey/post.ts
@@ -0,0 +1,6 @@
+export default {
+  title: "My React+Markdown blog journey (Part 1)",
+  tags: ["react", "markdown", "blog"],
+  spoiler: "How I (try to) create the perfect blog stack",
+  getContent: () => import("./document.mdx")
+};
