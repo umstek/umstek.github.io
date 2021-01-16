ad8fdd49f4068e41fee4f65b4f2d5149c1b3186c
Wickramaranga Abeygunawardhana <w@umstek.tk>
2021-01-16 15:41:59 +0530

    Fix CI (#25)
    * Add fixes for CI
    
    * Automatically configure git in action
    
    * Fix multiline command to config git
    
    * Use github key
    
    * Use github key
    
    * Use github key
    
    * Use global configs
    
    * Use PAT
    
    * Update main.yml
    
    * Restructure workflows
    
    * Fix env var names

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -14,25 +14,27 @@ const TagsPage = ({
       siteMetadata: { title },
     },
   },
-}) => (
-  <Layout location={location} title={title}>
-    <div className="mt-16">
-      <Helmet title={title} />
-      <div>
-        <h1>Tags</h1>
-        <div className="flex flex-wrap justify-center">
-          {group.map((tag) => (
-            <Tag
-              name={tag.fieldValue}
-              link={`/tags/${kebabCase(tag.fieldValue)}/`}
-              count={tag.totalCount}
-            />
-          ))}
+}) => {
+  return (
+    <Layout location={`/tags`} title={title}>
+      <div className="mt-16">
+        <Helmet title={title} />
+        <div>
+          <h1>Tags</h1>
+          <div className="flex flex-wrap justify-center">
+            {group.map((tag) => (
+              <Tag
+                name={tag.fieldValue}
+                link={`/tags/${kebabCase(tag.fieldValue)}/`}
+                count={tag.totalCount}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+};
 
 TagsPage.propTypes = {
   data: PropTypes.shape({
