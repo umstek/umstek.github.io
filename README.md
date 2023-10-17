99c30a9f903e4a13c251d0d6a9152f96c516e00f
Wickramaranga Abeygunawardhana <umstek@live.com>
2023-10-17 21:38:40 +0530

    feat(project): :tada: migrate to astro 3

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ![deployment](https://github.com/umstek/umstek.github.io/workflows/main/badge.svg)
 
 `main` branch contains the source code for new blog. All other branches are deprecated and abandoned.
+
+Template: [chrismwilliams/astro-theme-cactus](https://github.com/chrismwilliams/astro-theme-cactus) (MIT)
---
+++ astro.config.ts
@@ -0,0 +1,62 @@
+import { defineConfig } from "astro/config";
+import fs from "fs";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+import remarkShikiTwoslash from "remark-shiki-twoslash";
+import remarkUnwrapImages from "remark-unwrap-images";
+import { remarkReadingTime } from "./src/utils/remark-reading-time";
+
+// https://astro.build/config
+export default defineConfig({
+  // ! Please remember to replace the following site property with your own domain
+  site: "https://umstek.github.io/",
+  markdown: {
+    remarkPlugins: [
+      [remarkShikiTwoslash, { themes: ["github-dark", "github-light"] }],
+      remarkUnwrapImages,
+      remarkReadingTime,
+    ],
+    remarkRehype: { footnoteLabelProperties: { className: [""] } },
+  },
+  integrations: [
+    mdx({}),
+    react({
+      include: ["**/react/*"],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    prefetch(),
+  ],
+  image: {
+    domains: ["webmention.io"],
+  },
+  vite: {
+    plugins: [rawFonts([".ttf", ".woff", ".woff2"])],
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
+});
+
+function rawFonts(ext: Array<string>) {
+  return {
+    name: "vite-plugin-raw-fonts",
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore:next-line
+    transform(_, id) {
+      if (ext.some((e) => id.endsWith(e))) {
+        console.log(_, id);
+        const buffer = fs.readFileSync(id);
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null,
+        };
+      }
+    },
+  };
+}
